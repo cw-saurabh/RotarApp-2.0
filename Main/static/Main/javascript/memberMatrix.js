8bed4f99b338c3147189f6f6bdde2c736ad3f67c
ss0506 <ss.abcbe.0506@gmail.com>
2020-09-06 12:13:17 +0000

    Hotfixes 2

--- Main/static/Main/javascript/memberMatrix.js
+++ Main/static/Main/javascript/memberMatrix.js
@@ -1,7 +1,7 @@
 let matrix = {'Total':{'Male':0,'Female':0,'Others':0,'Total':0}};
 
 function updateMatrix(attributeId = null, column = null, subjectId = null ) {
-    
+
     if (attributeId!=null && column!=null && subjectId!=null)
     {
         value = $("#"+subjectId).val();
@@ -14,7 +14,7 @@ function updateMatrix(attributeId = null, column = null, subjectId = null ) {
         let Total = matrix[row]['Male']+matrix[row]['Female']+matrix[row]['Others'];
         matrix[row]['Total'] = Total;
         $("#M-"+row+"-T").val(Total);
-        
+
         if(matrix[row]['Operator']=='+')
         {
             matrix['Total']['Male']+=matrix[row]['Male'];
@@ -29,11 +29,11 @@ function updateMatrix(attributeId = null, column = null, subjectId = null ) {
             matrix['Total']['Others']-=matrix[row]['Others'];
             matrix['Total']['Total']-=matrix[row]['Total'];
         }
-    } 
+    }
     appendMemberMatrixTotalRow(matrix['Total']['Male'],matrix['Total']['Female'],matrix['Total']['Others'],matrix['Total']['Total']);
-    countN = (1 in matrix) ? matrix[1]['Total'] : null ;
-    countn = (2 in matrix) ? matrix[2]['Total'] : null ;
-    
+    countN = (6 in matrix) ? matrix[6]['Total'] : null ;
+    countn = (7 in matrix) ? matrix[7]['Total'] : null ;
+
     updateTotalNumberOfMembers();
     updateTotalNumberOfInductedMembers();
     updateOwedMoney();
