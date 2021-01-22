c40a22aea509ba605bb38d81a09682d1b681180d
ss0506 <ss.abcbe.0506@gmail.com>
2021-01-23 01:52:58 +0530

    Debug changed to 'False'

--- RotarApp/settings.py
+++ RotarApp/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv('KEY')
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['www.rotaractdistrict3131.org']
 
