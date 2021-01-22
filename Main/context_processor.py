2c16244a1b6092da1801aeb430f16346ca5486e9
ss0506 <ss.abcbe.0506@gmail.com>
2021-01-23 04:47:32 +0530

    Added dynamic avenueLinks in navbar

---
+++ Main/context_processor.py
@@ -0,0 +1,13 @@
+from Main.models import avenues, WhatWeDo
+
+def navbar_processor(request):
+    links = dict()
+    for avenue in avenues :
+        events = WhatWeDo.objects.filter(eventCategory=avenue).all()
+        if(len(events)):
+            links[avenue] = {
+                "Title" : avenues[avenue],
+                "SubTab" : avenues[avenue].replace(" ",'')
+            }
+    print(links)
+    return {'EventNavLinks': links}
